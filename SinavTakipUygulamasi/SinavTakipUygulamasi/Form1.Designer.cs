216d4c3474aa74f3fe8d52ff45de94a3ddbe8792
Işıl ceren satıcı <isilcerensatici2003@gmail.com>
2025-09-02 14:53:06 +0300

    This is a simple Windows Forms application developed as part of my Object-Oriented Programming course in my second year of university. The application allows users to track their exam dates and details in a structured and user-friendly interface.

---
+++ SinavTakipUygulamasi/SinavTakipUygulamasi/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SinavTakipUygulamasi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(36, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 0;
+
+            // textBox2
+            this.textBox2.Location = new System.Drawing.Point(36, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 1;
+
+            // button1
+            this.button1.Location = new System.Drawing.Point(36, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sınav Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            // button2
+            this.button2.Location = new System.Drawing.Point(154, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Yaklaşan Sınavlar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+
+            // listBox1
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 160);
+            this.listBox1.TabIndex = 4;
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Sınav Takip Uygulaması";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ SinavTakipUygulamasi/SinavTakipUygulamasi/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SinavTakipUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        private List<Sinav> sinavListesi = new List<Sinav>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dersAdi = textBox1.Text;
+            DateTime sinavTarihi;
+
+            // Tarih doğrulaması
+            if (DateTime.TryParse(textBox2.Text, out sinavTarihi))
+            {
+                // Listeye sınav ekleme
+                sinavListesi.Add(new Sinav(dersAdi, sinavTarihi));
+                // Listeyi güncelleme
+                listBox1.Items.Add($"{dersAdi} - {sinavTarihi:dd/MM/yyyy}");
+                // TextBoxları temizleme
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen geçerli bir tarih girin! (gg.aa.yyyy)", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime bugun = DateTime.Now;
+            var yaklasanSinavlar = sinavListesi.Where(s => s.Tarih >= bugun).OrderBy(s => s.Tarih).ToList();
+
+            if (yaklasanSinavlar.Any())
+            {
+                string mesaj = "Yaklaşan Sınavlar:\n";
+                foreach (var sinav in yaklasanSinavlar)
+                {
+                    mesaj += $"{sinav.DersAdi} - {sinav.Tarih:dd/MM/yyyy}\n";
+                }
+                MessageBox.Show(mesaj, "Yaklaşan Sınavlar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Yaklaşan sınav yok!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+
+    public class Sinav
+    {
+        public string DersAdi { get; set; }
+        public DateTime Tarih { get; set; }
+
+        public Sinav(string dersAdi, DateTime tarih)
+        {
+            DersAdi = dersAdi;
+            Tarih = tarih;
+        }
+    }
+}
